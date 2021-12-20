a0a8e3ef2612aa3a5e713458722c69d0e210a600
Justin <justin.ng147@gmail.com>
2021-12-20 02:17:01 -0800

    start web server

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,13 @@ import express from "express"
 import cors from "cors"
 import restaurants from "./api/restaurants.route.js"
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
-app.use("/api/v1/restaurants", restaurants);
-app.use("*", (req,res) => res.status(404).json({error: "not found"}));
+app.use("/api/v1/restaurants", restaurants)
+app.use("*", (req,res) => res.status(404).json({error: "not found"}))
 
-export default app;
+export default app
 
